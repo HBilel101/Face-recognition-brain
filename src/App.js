1c8c32f68a2ccd328ed789f95dafee75a26f23cd
HBilel101 <hbilel992@gmail.com>
2022-07-08 16:05:53 +0100

    solved increment only if image detected v2

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
   .then(response=>response.json())
 .then(data => {
         console.log('hi', data)
-        if (data.outputs) {
+        if (data.outputs != undefined) {
           fetch('https://friendly-crown-74163.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
